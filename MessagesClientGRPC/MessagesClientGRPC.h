227d8f40dcf75602375a17f611b00c2d4506b59a
Alexei Poponin <alexqw1@ya.ru>
2019-03-08 04:11:27 +0300

    Initial Commit

---
+++ MessagesClientGRPC/MessagesClientGRPC.h
@@ -0,0 +1,19 @@
+//
+//  MessagesClientGRPC.h
+//  MessagesClientGRPC
+//
+//  Created by Рабочий on 08/03/2019.
+//  Copyright © 2019 Рабочий. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessagesClientGRPC.
+FOUNDATION_EXPORT double MessagesClientGRPCVersionNumber;
+
+//! Project version string for MessagesClientGRPC.
+FOUNDATION_EXPORT const unsigned char MessagesClientGRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessagesClientGRPC/PublicHeader.h>
+
+
